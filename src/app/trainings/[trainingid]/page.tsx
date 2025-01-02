604fbe13fba7327c4525395a5853c571f0902ffc
chaebal <muhdadamyaqin@gmail.com>
2025-01-02 23:40:24 +0800

    added pagination

--- src/app/trainings/[trainingid]/page.tsx
+++ src/app/trainings/[trainingid]/page.tsx
@@ -11,7 +11,13 @@ import { BarChart } from "@mui/x-charts/BarChart";
 import { Video } from "lucide-react";
 import ImageChecking from "@/app/image_checking/page";
 import { Button } from "@nextui-org/button";
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  Pagination,
+} from "@nextui-org/react";
 import {
   Navbar,
   NavbarBrand,
@@ -76,6 +82,14 @@ const TrainingDetails = () => {
   const [downloadlink, setDownloadLink] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [videoUrl, setVideoUrl] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsPerPage = 3;
+
+  // Determine the current set of metrics based on the active page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentMetrics = metrics.slice(startIndex, endIndex);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -112,79 +126,6 @@ const TrainingDetails = () => {
     setIsModalVisible(false); // Hide the modal
     setVideoUrl(""); // Reset the video URL
   };
-
-  // const handleGetVideo = async () =>{
-  //   try{
-  //     const response = await fetch(
-  //               `/api/sqlite/playermetrics?player_id=${playerID}`
-  //             );
-  //   }
-  // }
-
-  // // Handle form submission
-  // const handleFormSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-
-  //   // const playerMetrics: Stats = {
-  //   //   player_id: 1,
-  //   //   sess: 101,
-  //   //   speed: 5.0,
-  //   //   distance: 500,
-  //   //   time: 100,
-  //   // };
-
-  //   // Function to send metrics asynchronously
-  //   const sendPlayerMetrics = async () => {
-  //     try {
-  //       const response = await fetch("/api/sqlite/playermetrics", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(playerMetrics),
-  //       });
-
-  //       const data = await response.json();
-
-  //       if (!response.ok) {
-  //         throw new Error(data.error || "Error updating player metrics");
-  //       }
-
-  //       setMessage("Metrics updated successfully!");
-  //       setError(null);
-  //     } catch (error: any) {
-  //       setMessage(null);
-  //       setError(error.message || "Failed to update metrics");
-  //     }
-  //   };
-
-  //   sendPlayerMetrics();
-  // };
-
-  // const handleGetPlayerMetrics = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-
-  //   const fetchMetrics = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `/api/sqlite/playermetrics?player_id=${playerID}`
-  //       );
-  //       const data = await response.json();
-
-  //       if (!response.ok) {
-  //         throw new Error(data.error || "Failed to fetch metrics.");
-  //       }
-
-  //       setMetrics(data.metrics);
-  //       setError(null);
-  //       // console.log("Metrics: " + metrics?.player_id);
-  //     } catch (err: any) {
-  //       setError(err.message);
-  //     }
-  //   };
-
-  //   fetchMetrics();
-  // };
 
   return (
     <div>
@@ -225,61 +166,46 @@ const TrainingDetails = () => {
         </div>
       </header>
 
-      {/* <div>
-        <form onSubmit={handleFormSubmit}>
-          <button type="submit">Submit</button>
-        </form>
-        {message && <p>{message}</p>}
-        {error && <p>{error}</p>}
-      </div> */}
       <div>
-        {/* <form onSubmit={handleGetPlayerMetrics}>
-          <input
-            placeholder="Player ID"
-            type="number"
-            onChange={(e) => setPlayerID(Number(e.target.value))}
-          ></input>
-          <button type="submit">Retrieve</button> */}
-        {/* </form> */}
         <div className="top-section mt-2 flex justify-center items-center w-full h-full">
           <div className="flex flex-col w-3/4 border border-dashed border-gray-400 p-4 h-64 items-center justify-center rounded-lg">
             <ImageChecking />
           </div>
         </div>
         <div className="flex flex-2 flex-col gap-4 p-4 pt-0 mt-3 w-full items-center">
-          {metrics && metrics.length > 0 ? (
-            metrics.map((metric, index) => (
+          {/* {metrics && metrics.length > 0 ? ( */}
+          {currentMetrics && currentMetrics.length > 0 ? (
+            currentMetrics.map((metric, index) => (
+              // metrics.map((metric, index) => (
               <React.Fragment key={index}>
                 <Card className="py-4 bg-gradient-to-br from-purple-900 to-black-200 w-3/4">
-                  <div className="flex-col">
+                  <div className="flex-col gap-4">
                     <h1 className="text-center text-2xl font-bold mb-5">
                       Summary
                     </h1>
                     <CardHeader className="pb-0 pt-2 px-4 items-start justify-between">
                       <div>
-                        {/* <p className="text-tiny uppercase font-bold">
-                          Session {metric.session_no}
-                        </p> */}
-                        <small className="text-default-500">12 Tracks</small>
-                        <h4 className="font-bold text-large truncate">
+                        <h4 className="font-bold text-2xl sm:text-md md:text-lg lg:text-xl xl:text-2xl truncate">
                           Session {metric.session_no}
                         </h4>
                       </div>
                       <div className="w-full text-center">
                         <h1 className="text-sm">Average Speed</h1>
-                        <p className="text-4xl mt-3">{`${metric.speed.toFixed(
+                        <p className="text-2xl sm:text-md md:text-xl lg:text-3xl xl:text-4xl mt-3">{`${metric.speed.toFixed(
                           2
                         )} m/s`}</p>
                       </div>
                       <div className="w-full text-center">
                         <h1 className="text-sm">Total Distance Covered</h1>
-                        <p className="text-4xl mt-3">
+                        <p className="text-2xl sm:text-md md:text-xl lg:text-3xl xl:text-4xl mt-3">
                           {`${metric.distance.toFixed(2)} m`}
                         </p>
                       </div>
                       <div className="w-full text-center">
                         <h1 className="text-sm">Time Taken</h1>
-                        <p className="text-4xl mt-3">{metric.time} s</p>
+                        <p className="text-2xl sm:text-md md:text-xl lg:text-3xl xl:text-4xl mt-3">
+                          {metric.time} s
+                        </p>
                       </div>
                     </CardHeader>
                   </div>
@@ -287,15 +213,13 @@ const TrainingDetails = () => {
                     <Image
                       alt="Card background"
                       className="object-cover rounded-xl"
-                      // src="http://127.0.0.1:8000/frame/solo.mp4_frame.jpg"
-                      // src={`http://127.0.0.1:8000/frame/${frameId}`}
                       src={metric.frame_url}
                       width={140}
                     />
-                    <div className="flex-1 flex flex-col justify-end items-center">
+                    <div className="flex-1 flex flex-col justify-end items-center h-">
                       <div className="flex space-x-4">
                         <Button
-                          className="bg-blue-500 text-white px-4 py-2 rounded"
+                          className="bg-blue-500 text-white px-4 py-2 rounded "
                           onClick={handleViewClick}
                         >
                           View
@@ -317,13 +241,7 @@ const TrainingDetails = () => {
                               >
                                 ✕
                               </Button>
-                              {/* <video
-                                controls
-                                width="600"
-                                // src={videoUrl}
-                                src="http://127.0.0.1:8000/download/solo_drill_1_detections.mp4"
-                                className="rounded-md"
-                              > */}
+
                               <video controls width="600">
                                 <source
                                   src="http://127.0.0.1:8000/view/solo_drill_1_detections.mp4"
@@ -331,8 +249,6 @@ const TrainingDetails = () => {
                                 />
                                 Your browser does not support the video tag.
                               </video>
-                              {/* Your browser does not support the video tag.
-                              </video> */}
                             </div>
                           </div>
                         )}
@@ -357,162 +273,14 @@ const TrainingDetails = () => {
               <p>No data available</p>
             </div>
           )}
-
-          {/* <>
-            <Card className="py-4 bg-gradient-to-br from-purple-900 to-black-200 w-3/4">
-              <div className="flex-col">
-                <CardHeader className="pb-0 pt-2 px-4 items-start justify-between">
-                  <div>
-                    <p className="text-tiny uppercase font-bold">Session 2</p>
-                    <small className="text-default-500">12 Tracks</small>
-                    <h4 className="font-bold text-large">Slalom Dribbling</h4>
-                  </div>
-                  <div className="w-full text-center">
-                    <h1 className="text-sm">Average Speed</h1>
-                    <p className="text-4xl mt-3">24 m/s</p>
-                  </div>
-                  <div className="w-full text-center">
-                    <h1 className="text-sm">Total Distance Covered</h1>
-                    <p className="text-4xl mt-3">65 m</p>
-                  </div>
-                  <div className="w-full text-center">
-                    <h1 className="text-sm">Time Taken</h1>
-                    <p className="text-4xl mt-3">20 s</p>
-                  </div>
-                </CardHeader>
-              </div>
-              <CardBody className="overflow-visible py-2 flex flex-row">
-                <Image
-                  alt="Card background"
-                  className="object-cover rounded-xl"
-                  src="https://nextui.org/images/hero-card-complete.jpeg"
-                  width={140}
-                />
-                <div className="flex-1 flex flex-col justify-end items-center">
-                  <div className="flex space-x-4">
-                    <Button>View</Button>
-                    <Button>Download</Button>
-                    <Button>More</Button>
-                  </div>
-                </div>
-              </CardBody>
-            </Card>
-          </>
-          <>
-            <Card className="py-4 bg-gradient-to-l from-purple-900 to-black-200">
-              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold">Session 2</p>
-                <small className="text-default-500">12 Tracks</small>
-                <h4 className="font-bold text-large">Slalom Dribbling</h4>
-              </CardHeader>
-              <CardBody className="overflow-visible py-2 flex flex-row justify-between">
-                <Image
-                  alt="Card background"
-                  className="object-cover rounded-xl"
-                  src="https://nextui.org/images/hero-card-complete.jpeg"
-                  width={270}
-                />
-                <div>
-                  <h1>Average Speed</h1>
-                </div>
-                <div>
-                  <h1>Total Distance Covered</h1>
-                </div>
-                <div>
-                  <h1>Time Taken</h1>
-                </div>
-              </CardBody>
-            </Card>
-          </> */}
-          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3 ">
-            <Card>
-              <CardContent className="p-5 rounded-lg shadow-md text-center">
-                Avatar
-              </CardContent>
-            </Card>
-            <div className="col-span-2 flex flex-row w-full justify-between">
-              <Card className="flex-1 text-center rounded-none">
-                <CardContent className="p-5 shadow-md text-center">
-                  <div>Total Training Time</div>
-                  <div className="text-5xl mt-2">23</div>
-                </CardContent>
-              </Card>
-              <Card className="flex-1 text-center rounded-none">
-                <CardContent className="p-5  shadow-md text-center">
-                  <div>Total Distance Covered</div>
-                  <div className="text-5xl mt-2">23 m</div>
-                </CardContent>
-              </Card>
-              <Card className="flex-1 text-center rounded-none">
-                <CardContent className="p-5 shadow-md text-center">
-                  <div>Average Speed</div>
-                  <div className="text-5xl mt-2">23 m/s</div>
-                </CardContent>
-              </Card>
-            </div>
-            <div>
-              <ImageChecking />
-            </div>
-          </div> */}
         </div>
-
-        {/* <div className="flex gap-4">
-          <div className="p-4 bg-gray-200">Test 1</div>
-          <div className="p-4 bg-gray-200">Test 2</div>
-        </div> */}
-
-        {/* <div className="flex items-center justify-center min-h-screen">
-          <div className="grid grid-cols-2 gap-2 w-1/4 max-w-4xl bg-gray-800 bg-opacity-80 p-6 rounded-lg shadow-md">
-            {metrics && metrics.length > 0 ? (
-              metrics.map((metric, index) => (
-                <React.Fragment key={index}>
-                  <Card>
-                    <CardContent className="p-5 rounded-lg shadow-md text-center">
-                      <div className="text-2xl mt-2">{metric.training_id}</div>
-                      <div className="text-sm font-bold text-gray-500">
-                        Training ID
-                      </div>
-                    </CardContent>
-                  </Card>
-
-                  <Card>
-                    <CardContent className="p-5 rounded-lg shadow-md text-center">
-                      <IoIosSpeedometer className="mx-auto text-lg text-gray-300" />
-                      <div className="text-2xl mt-2">{metric.speed}</div>
-                      <div className="text-sm font-bold text-gray-500">
-                        Speed
-                      </div>
-                    </CardContent>
-                  </Card>
-
-                  <Card>
-                    <CardContent className="p-5 rounded-lg shadow-md text-center">
-                      <IoLocationSharp className="mx-auto text-lg text-gray-300" />
-                      <div className="text-2xl mt-2">{metric.distance}</div>
-                      <div className="text-sm font-bold text-gray-500">
-                        Distance
-                      </div>
-                    </CardContent>
-                  </Card>
-
-                  <Card>
-                    <CardContent className="p-5 rounded-lg shadow-md text-center">
-                      <FaClock className="mx-auto text-sm text-gray-300" />
-                      <div className="text-2xl mt-2">{metric.time}</div>
-                      <div className="text-sm font-bold text-gray-500">
-                        Time
-                      </div>
-                    </CardContent>
-                  </Card>
-                </React.Fragment>
-              ))
-            ) : (
-              <div className="col-span-2 text-center text-white">
-                <p>No data available</p>
-              </div>
-            )}
-          </div>
-        </div> */}
+        <div className="mt-4 flex justify-center items-center">
+          <Pagination
+            initialPage={1}
+            total={Math.ceil(metrics.length / itemsPerPage)}
+            onChange={(page) => setCurrentPage(page)}
+          />
+        </div>
       </div>
     </div>
   );
